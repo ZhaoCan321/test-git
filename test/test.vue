1cc241f6aec94172311e649aa445795de0b7076c
Bike <923663860@qq.com>
2020-12-29 19:11:49 +0800

    test-1

---
+++ test/test.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: zz
+ * @Date: 2020-12-29 19:10:46
+ * @LastEditors: ***
+ * @LastEditTime: 2020-12-29 19:10:54
+-->
+<template>
+  <div>
+    test
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  }
+}
+</script>
+<style lang="scss" scoped>
+  
+</style>
