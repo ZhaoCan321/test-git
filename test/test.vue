0ea538ddec9972532f9b6f2cb898e31596f15961
Bike <923663860@qq.com>
2020-12-29 21:01:07 +0800

    test-2-reset

--- test/test.vue
+++ test/test.vue
@@ -2,7 +2,7 @@
  * @Author: zz
  * @Date: 2020-12-29 19:10:46
  * @LastEditors: ***
- * @LastEditTime: 2020-12-29 20:55:46
+ * @LastEditTime: 2020-12-29 20:59:16
 -->
 <template>
   <div>
@@ -10,7 +10,6 @@
     test-1-0
     test-1-1
     test-2-0
-    test-3-0
   </div>
 </template>
 <script>
