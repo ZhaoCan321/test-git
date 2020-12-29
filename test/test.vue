5bade302060b6fd100cd6073ed56d57ff5e9c82a
Bike <923663860@qq.com>
2020-12-29 20:53:23 +0800

    test-3

--- test/test.vue
+++ test/test.vue
@@ -2,7 +2,7 @@
  * @Author: zz
  * @Date: 2020-12-29 19:10:46
  * @LastEditors: ***
- * @LastEditTime: 2020-12-29 20:36:09
+ * @LastEditTime: 2020-12-29 20:53:14
 -->
 <template>
   <div>
@@ -10,6 +10,7 @@
     test-1-0
     test-1-1
     test-2-0
+    test-3-0
   </div>
 </template>
 <script>
